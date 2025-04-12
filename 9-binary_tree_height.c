94f76199220048307df14153b01ab070b3ecb0b4
kamisos3 <kamilasostre11@gmail.com>
2025-04-11 23:54:33 -0400

    Measures height of binary tree

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -17,8 +17,14 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	{
 		return (0);
 	}
+
 	left_height = binary_tree_height(tree->left);
 	right_height = binary_tree_height(tree->right);
+
+	if (left_height == 0 && right_height == 0)
+	{
+		return (0);
+	}
 
 	return ((left_height > right_height ? left_height : right_height) + 1);
 }
