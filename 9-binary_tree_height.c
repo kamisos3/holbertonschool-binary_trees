13f800ac90fe8d12ace2f14c6af66e0bbe3159fd
kamisos3 <kamilasostre11@gmail.com>
2025-04-12 22:58:01 -0400

    Checks height of binary tree

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -4,27 +4,41 @@
 * @tree: pointer to root node of tree
 *
 *
-*
-*
 * Return: 0 if tree is NULL
 */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left_height;
-	size_t right_height;
+	size_t left;
+	size_t right;
 
 	if (tree == NULL)
 	{
 		return (0);
 	}
 
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
-
-	if (left_height == 0 && right_height == 0)
+	if (tree->left == NULL && tree->right == NULL)
 	{
 		return (0);
 	}
 
-	return ((left_height > right_height ? left_height : right_height) + 1);
+	if (tree->left != NULL)
+	{
+		left = 1 + binary_tree_height(tree->left);
+	}
+	if (tree->right != NULL)
+	{
+		right = 1 + binary_tree_height(tree->right);
+	}
+	else
+	right = 0;
+
+	if (left > right)
+	{
+		return (left);
+	}
+
+	else
+{
+		return (right);
+	}
 }
