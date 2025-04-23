d20bf9a65c942dc8d85d56297f022f654ad6b42f
kamisos3 <kamilasostre11@gmail.com>
2025-04-22 22:41:48 -0400

    Setting value to 0 in height function

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -44,8 +44,8 @@ size_t binary_tree_size(const binary_tree_t *tree)
 */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left;
-	size_t right;
+	size_t left = 0;
+	size_t right = 0;
 
 	if (tree == NULL)
 	{
